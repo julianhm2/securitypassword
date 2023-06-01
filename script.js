d6d0cabd5de1271e3dc681fb66f0af75421c5500
JulianHM <julianhoyos96@gmail.com>
2023-06-01 04:18:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function generatePassword() {
+  var length = parseInt(document.getElementById("longitud").value);
+  var charset = "abcdefghijklmnopqrstuvwxyz";
+  var password = "";
+
+  if (document.getElementById("caracteresesp").checked) {
+    charset += "!@#$%^&*()";
+  }
+
+  if (document.getElementById("numeros").checked) {
+    charset += "0123456789";
+  }
+
+  if (document.getElementById("simbolos").checked) {
+    charset += "~`-_=+[{]}\\|;:'\",<.>/?";
+  }
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  document.getElementById("contraseña").value = password;
+}
