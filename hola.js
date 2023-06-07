4ac28ee62a5563503a4bd5a9d842b880ae2f55aa
JulianHM <julianhoyos96@gmail.com>
2023-06-07 07:55:16 -0500

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,8 @@
+function toggleOptions() {
+  var opciones = document.getElementById("opciones");
+  if (opciones.style.display === "none") {
+    opciones.style.display = "block";
+  } else {
+    opciones.style.display = "none";
+  }
+}
